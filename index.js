0e174ebcf54ee4bac1320ae2b7771699ca6ffeff
doanvanquoc <vanquoc.dev@gmail.com>
2024-05-25 14:01:08 +0700

    fix cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const setupSwagger = require('./swagger');
+const cors = require('cors')
 const PORT = process.env.PORT || 8080
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
