7baccfd0a8e2eb20f4269697458f45ccf226ff14
doanvanquoc <vanquoc.dev@gmail.com>
2024-07-15 01:54:20 +0700

    thêm verify token khi thanh toán chuyển khoản

--- routes/thanh_toan.js
+++ routes/thanh_toan.js
@@ -3,6 +3,6 @@ const controller = require('../controllers/thanh_toan')
 const router = express.Router()
 const verify_token = require('../middlewares/verify_token')
 
-router.post('/', controller.thanhToanChuyenKhoan)
+router.post('/', verify_token, controller.thanhToanChuyenKhoan)
 
 module.exports = router
